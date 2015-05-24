75b35a83ad407cfb3393a6daf12fbfd22d1de7ec
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-05-24 15:55:33 -0700

    4. State vs Props: Solution - Made states in sub component into props received from parent's state.

--- exercises/4-props-vs-state/app.js
+++ exercises/4-props-vs-state/app.js
@@ -18,22 +18,18 @@ var data = require('./data');
 var Tabs = React.createClass({
 
   propTypes: {
-    data: React.PropTypes.array.isRequired
-  },
-
-  getInitialState () {
-    return {
-      activeTabIndex: 0
-    };
+    data: React.PropTypes.array.isRequired,
+    activeTabIndex: React.PropTypes.number.isRequired,
+    onActivateTab: React.PropTypes.func.isRequired
   },
 
   handleTabClick (activeTabIndex) {
-    this.setState({activeTabIndex});
+    this.props.onActivateTab(activeTabIndex);
   },
 
   renderTabs () {
     return this.props.data.map((tab, index) => {
-      var style = this.state.activeTabIndex === index ?
+      var style = this.props.activeTabIndex === index ?
         styles.activeTab : styles.tab;
       var clickHandler = this.handleTabClick.bind(this, index);
       return (
@@ -45,7 +41,7 @@ var Tabs = React.createClass({
   },
 
   renderPanel () {
-    var tab = this.props.data[this.state.activeTabIndex];
+    var tab = this.props.data[this.props.activeTabIndex];
     return (
       <div>
         <p>{tab.description}</p>
@@ -68,14 +64,29 @@ var Tabs = React.createClass({
 });
 
 var App = React.createClass({
+  getInitialState () {
+    return {
+      activeTabIndex: 0
+    };
+  },
+
+  handleActivateTab (activeTabIndex) {
+    this.setState({ activeTabIndex });
+  },
+
   render () {
     return (
       <div>
         <h1>Props v. State</h1>
-        <Tabs data={this.props.tabs}/>
+        <Tabs
+          data={this.props.tabs}
+          activeTabIndex={this.state.activeTabIndex}
+          onActivateTab={this.handleActivateTab}
+          />
       </div>
     );
   }
 });
 
 React.render(<App tabs={data}/>, document.body);
+
